44e01a7bd103078c96b30de3fdbf1778bad36954
theifel <condecrom@gmail.com>
2021-06-20 18:52:58 +0300

    Added signup usecase draft

---
+++ dashboard/src/Model/User/UseCase/SignUp/Request/Command.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Model\User\UseCase\SignUp\Request;
+
+class Command
+{
+    public string $email;
+    public string $password;
+}
---
+++ dashboard/src/Model/User/UseCase/SignUp/Request/Handler.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Model\User\UseCase\SignUp\Request;
+
+class Handler
+{
+    public function handle(Command $command): void
+    {
+
+    }
+}
