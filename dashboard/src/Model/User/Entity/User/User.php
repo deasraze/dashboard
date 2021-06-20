d76aeea31667d45872b95f8e6be52cc81d012499
theifel <condecrom@gmail.com>
2021-06-20 20:39:50 +0300

    Added user uuid

--- dashboard/src/Model/User/Entity/User/User.php
+++ dashboard/src/Model/User/Entity/User/User.php
@@ -4,13 +4,20 @@ namespace App\Model\User\Entity\User;
 
 class User
 {
+    private string $id;
     private string $email;
     private string $password;
 
-    public function __construct(string $email, string $password)
+    public function __construct(string $id, string $email, string $password)
     {
+        $this->id = $id;
         $this->email = $email;
         $this->password = $password;
+    }
+
+    public function getId(): string
+    {
+        return $this->id;
     }
 
     public function getEmail(): string
