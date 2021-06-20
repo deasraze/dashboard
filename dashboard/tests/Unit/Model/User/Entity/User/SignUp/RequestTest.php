0c3de2e93e3a5f372dfe1d3bb3e17f55834ec6a8
theifel <condecrom@gmail.com>
2021-06-20 18:59:47 +0300

    Added draft signup request test

---
+++ dashboard/tests/Unit/Model/User/Entity/User/SignUp/RequestTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Tests\Unit\Model\User\Entity\User\SignUp;
+
+use PHPUnit\Framework\TestCase;
+
+class RequestTest extends TestCase
+{
+    public function testSuccess(): void
+    {
+    }
+}
