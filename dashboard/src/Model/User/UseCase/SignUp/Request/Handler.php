389cd5736cc5a9a57a26389aeb58fb9d9d51d568
theifel <condecrom@gmail.com>
2021-06-20 19:23:00 +0300

    Updated signup request handler

--- dashboard/src/Model/User/UseCase/SignUp/Request/Handler.php
+++ dashboard/src/Model/User/UseCase/SignUp/Request/Handler.php
@@ -2,10 +2,32 @@
 
 namespace App\Model\User\UseCase\SignUp\Request;
 
+use App\Model\User\Entity\User\User;
+use Doctrine\ORM\EntityManagerInterface;
+
 class Handler
 {
+    private EntityManagerInterface $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
     public function handle(Command $command): void
     {
+        $email = \mb_strtolower($command->email);
 
+        if ($this->em->getRepository(User::class)->findOneBy(['email' => $email])) {
+            throw new \DomainException('User with this email is already registered.');
+        }
+
+        $user = new User(
+            $email,
+            \password_hash($command->password, PASSWORD_DEFAULT)
+        );
+
+        $this->em->persist($user);
+        $this->em->flush();
     }
 }
