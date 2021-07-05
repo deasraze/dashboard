2669fffaf7740f33663c553afe2f0fcfaa03644b
deasraze <condecrom@gmail.com>
2021-07-06 01:08:47 +0300

    Implemented reset token sender

--- dashboard/src/Model/User/Service/ResetTokenSender.php
+++ dashboard/src/Model/User/Service/ResetTokenSender.php
@@ -4,8 +4,32 @@ namespace App\Model\User\Service;
 
 use App\Model\User\Entity\User\Email;
 use App\Model\User\Entity\User\ResetToken;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
 
-interface ResetTokenSender
+class ResetTokenSender
 {
-    public function send(Email $email, ResetToken $token): void;
+    private MailerInterface $mailer;
+    private array $from;
+
+    public function __construct(MailerInterface $mailer, array $from)
+    {
+        $this->mailer = $mailer;
+        $this->from = $from;
+    }
+
+    public function send(Email $email, ResetToken $token): void
+    {
+        $email = (new TemplatedEmail())
+            ->from(new Address(...$this->from))
+            ->to($email->getValue())
+            ->subject('Password resetting')
+            ->htmlTemplate('mail/user/reset.html.twig')
+            ->context([
+                'token' => $token->getToken(),
+            ]);
+
+        $this->mailer->send($email);
+    }
 }
