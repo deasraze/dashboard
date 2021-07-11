7255fe45c99c90ffeee9c26b013b44b1855912d3
deasraze <condecrom@gmail.com>
2021-07-11 17:22:45 +0300

    Added logout

--- dashboard/src/Controller/Auth/AuthController.php
+++ dashboard/src/Controller/Auth/AuthController.php
@@ -25,4 +25,12 @@ class AuthController extends AbstractController
 
         return $this->render('app/auth/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
+
+    /**
+     * @Route("/logout", name="app_logout")
+     */
+    public function logout()
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
 }
