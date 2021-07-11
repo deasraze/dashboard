b287d293ff6f37c37171fbae6fe7249c7c101f9c
deasraze <condecrom@gmail.com>
2021-07-11 13:07:34 +0300

    Added global email sender

--- dashboard/src/Model/User/Service/ResetTokenSender.php
+++ dashboard/src/Model/User/Service/ResetTokenSender.php
@@ -6,23 +6,19 @@ use App\Model\User\Entity\User\Email;
 use App\Model\User\Entity\User\ResetToken;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\Mailer\MailerInterface;
-use Symfony\Component\Mime\Address;
 
 class ResetTokenSender
 {
     private MailerInterface $mailer;
-    private array $from;
 
-    public function __construct(MailerInterface $mailer, array $from)
+    public function __construct(MailerInterface $mailer)
     {
         $this->mailer = $mailer;
-        $this->from = $from;
     }
 
     public function send(Email $email, ResetToken $token): void
     {
         $email = (new TemplatedEmail())
-            ->from(new Address(...$this->from))
             ->to($email->getValue())
             ->subject('Password resetting')
             ->htmlTemplate('mail/user/reset.html.twig')
