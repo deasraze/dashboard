c664088135e25b68a5fddf6f2ffd94905192b38a
deasraze <condecrom@gmail.com>
2021-07-20 17:09:02 +0300

    Added remember me badge

--- dashboard/src/Security/LoginFormAuthenticator.php
+++ dashboard/src/Security/LoginFormAuthenticator.php
@@ -12,6 +12,7 @@ use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\RememberMeBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\CustomCredentials;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
@@ -51,6 +52,7 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
             $credentials,
             [
                 new CsrfTokenBadge('authenticate', $request->get('_csrf_token')),
+                new RememberMeBadge(),
             ]
         );
     }
