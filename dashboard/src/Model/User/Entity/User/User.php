57d6d383753cde6baaaf0fc5826ca2e3259bf3d2
deasraze <condecrom@gmail.com>
2021-07-22 00:04:31 +0300

    Removed user new status

--- dashboard/src/Model/User/Entity/User/User.php
+++ dashboard/src/Model/User/Entity/User/User.php
@@ -17,7 +17,6 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class User
 {
-    private const STATUS_NEW = 'new';
     private const STATUS_WAIT = 'wait';
     public const STATUS_ACTIVE = 'active';
 
@@ -136,11 +135,6 @@ class User
         }
 
         $this->role = $role;
-    }
-
-    public function isNew(): bool
-    {
-        return ($this->status === self::STATUS_NEW);
     }
 
     public function isActive(): bool
