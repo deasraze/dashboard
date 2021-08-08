c10336f685c438d1eaa561d218da7f4c437208c9
deasraze <condecrom@gmail.com>
2021-08-09 00:50:37 +0300

    Added domain logic to work member

--- dashboard/src/Model/Work/Entity/Members/Member/Member.php
+++ dashboard/src/Model/Work/Entity/Members/Member/Member.php
@@ -35,4 +35,77 @@ class Member
      * @ORM\Column(type="work_members_member_status", length=16)
      */
     private Status $status;
+
+    public function __construct(Id $id, Group $group, Name $name, Email $email)
+    {
+        $this->id = $id;
+        $this->group = $group;
+        $this->name = $name;
+        $this->email = $email;
+        $this->status = Status::active();
+    }
+
+    public function edit(Name $name, Email $email): void
+    {
+        $this->name = $name;
+        $this->email = $email;
+    }
+
+    public function move(Group $group): void
+    {
+        $this->group = $group;
+    }
+
+    public function archive(): void
+    {
+        if ($this->status->isArchived()) {
+            throw new \DomainException('Member is already archived.');
+        }
+
+        $this->status = Status::archived();
+    }
+
+    public function reinstate(): void
+    {
+        if ($this->status->isActive()) {
+            throw new \DomainException('Member is already active.');
+        }
+
+        $this->status = Status::active();
+    }
+
+    public function isActive(): bool
+    {
+        return $this->status->isActive();
+    }
+
+    public function isArchived(): bool
+    {
+        return $this->status->isArchived();
+    }
+
+    public function getId(): Id
+    {
+        return $this->id;
+    }
+
+    public function getGroup(): Group
+    {
+        return $this->group;
+    }
+
+    public function getName(): Name
+    {
+        return $this->name;
+    }
+
+    public function getEmail(): Email
+    {
+        return $this->email;
+    }
+
+    public function getStatus(): Status
+    {
+        return $this->status;
+    }
 }
