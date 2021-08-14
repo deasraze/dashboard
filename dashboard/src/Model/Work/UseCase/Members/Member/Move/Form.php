bfa0fc2d35d81855dde67fd918313af7cbe0a5f3
deasraze <condecrom@gmail.com>
2021-08-14 20:09:37 +0300

    Added move member use case

---
+++ dashboard/src/Model/Work/UseCase/Members/Member/Move/Form.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Model\Work\UseCase\Members\Member\Move;
+
+use App\ReadModel\Work\Members\GroupFetcher;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class Form extends AbstractType
+{
+    private GroupFetcher $groups;
+
+    public function __construct(GroupFetcher $groups)
+    {
+        $this->groups = $groups;
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('group', ChoiceType::class, ['choices' => \array_flip($this->groups->assoc())]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Command::class,
+        ]);
+    }
+}
