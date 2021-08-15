65ddb9551ef7391803c2ea3b34f4a509f00f7cca
deasraze <condecrom@gmail.com>
2021-08-15 16:02:09 +0300

    Added check for empty before deleting group

--- dashboard/src/Model/Work/Entity/Members/Member/MemberRepository.php
+++ dashboard/src/Model/Work/Entity/Members/Member/MemberRepository.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Model\Work\Entity\Members\Member;
 
 use App\Model\EntityNotFoundException;
+use App\Model\Work\Entity\Members\Group\Id as GroupId;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\EntityRepository;
 
@@ -34,6 +35,15 @@ class MemberRepository
                 ->select('COUNT(t.id)')
                 ->where('t.id = :id')
                 ->setParameter(':id', $id->getValue())
+                ->getQuery()->getSingleScalarResult() > 0;
+    }
+
+    public function hasByGroup(GroupId $id): bool
+    {
+        return $this->repo->createQueryBuilder('t')
+                ->select('COUNT(t.id)')
+                ->where('t.group = :group')
+                ->setParameter(':group', $id->getValue())
                 ->getQuery()->getSingleScalarResult() > 0;
     }
 
