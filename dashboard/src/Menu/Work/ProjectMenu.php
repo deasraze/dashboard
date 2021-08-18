66c99c2a8b136b6f78ab64892df305d8181bb8a3
deasraze <condecrom@gmail.com>
2021-08-18 10:26:42 +0300

    Added projects tabs

---
+++ dashboard/src/Menu/Work/ProjectMenu.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Menu\Work;
+
+use Knp\Menu\FactoryInterface;
+use Knp\Menu\ItemInterface;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+
+class ProjectMenu
+{
+    private FactoryInterface $factory;
+    private AuthorizationCheckerInterface $auth;
+
+    public function __construct(FactoryInterface $factory, AuthorizationCheckerInterface $auth)
+    {
+        $this->factory = $factory;
+        $this->auth = $auth;
+    }
+
+    public function build(array $options): ItemInterface
+    {
+        $menu = $this->factory->createItem('root')
+            ->setChildrenAttribute('class', 'nav nav-tabs mb-4');
+
+        $menu
+            ->addChild('Dashboard', [
+                'route' => 'work.projects.project.show',
+                'routeParameters' => ['id' => $options['project_id']]
+            ])
+            ->setAttribute('class', 'nav-item')
+            ->setLinkAttribute('class', 'nav-link')
+            ->setExtra('routes', [
+                'route' => 'work.projects.project.show',
+                'pattern' => '/^work\.projects\.project\.show\..+/',
+            ]);
+
+        if ($this->auth->isGranted('ROLE_WORK_MANAGE_PROJECTS')) {
+            $menu
+                ->addChild('Settings', [
+                    'route' => 'work.projects.project.settings',
+                    'routeParameters' => ['project_id' => $options['project_id']]
+                ])
+                ->setAttribute('class', 'nav-item')
+                ->setLinkAttribute('class', 'nav-link')
+                ->setExtra('routes', [
+                    'route' => 'work.projects.project.settings',
+                    'pattern' => '/^work\.projects\.project\.settings\..+/',
+                ]);
+        }
+
+        return $menu;
+    }
+}
