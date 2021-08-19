91d2677fbea08d3bf9bc3399e017bc05fbdfc465
deasraze <condecrom@gmail.com>
2021-08-19 09:31:30 +0300

    Added projects controllers

---
+++ dashboard/src/ReadModel/Work/Projects/Project/Filter/Filter.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\ReadModel\Work\Projects\Project\Filter;
+
+use App\Model\Work\Entity\Projects\Project\Status;
+
+class Filter
+{
+    public ?string $name = null;
+    public ?string $status = Status::ACTIVE;
+}
