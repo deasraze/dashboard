c4605a7bb5f14ee8a467c6152eb75441fb6f5e0b
deasraze <condecrom@gmail.com>
2021-08-23 16:50:29 +0300

    Added project task edit

---
+++ dashboard/tests/Builder/Work/Projects/TaskBuilder.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Builder\Work\Projects;
+
+use App\Model\Work\Entity\Members\Member\Member;
+use App\Model\Work\Entity\Projects\Project\Project;
+use App\Model\Work\Entity\Projects\Task\Id;
+use App\Model\Work\Entity\Projects\Task\Task;
+use App\Model\Work\Entity\Projects\Task\Type;
+
+class TaskBuilder
+{
+    private Id $id;
+    private \DateTimeImmutable $date;
+    private Type $type;
+    private int $priority;
+    private string $name;
+    private string $content;
+
+    public function __construct()
+    {
+        $this->id       = new Id(1);
+        $this->date     = new \DateTimeImmutable();
+        $this->type     = new Type(Type::FEATURE);
+        $this->priority = 1;
+        $this->name     = 'Test Task';
+        $this->content  = 'Test Content';
+    }
+
+    public function build(Project $project, Member $author): Task
+    {
+        return new Task(
+            $this->id,
+            $project,
+            $author,
+            $this->date,
+            $this->type,
+            $this->priority,
+            $this->name,
+            $this->content
+        );
+    }
+}
