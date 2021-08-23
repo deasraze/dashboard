eefa23842d4e8d9e1fe3ca6b468440503504ba64
deasraze <condecrom@gmail.com>
2021-08-23 16:36:41 +0300

    Added projects task create

---
+++ dashboard/tests/Builder/Work/Projects/ProjectBuilder.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Builder\Work\Projects;
+
+use App\Model\Work\Entity\Projects\Project\Id;
+use App\Model\Work\Entity\Projects\Project\Project;
+
+class ProjectBuilder
+{
+    private string $name;
+    private int $sort;
+
+    public function __construct()
+    {
+        $this->name = 'Project';
+        $this->sort = 1;
+    }
+
+    public function build(): Project
+    {
+        return new Project(
+            Id::next(),
+            $this->name,
+            $this->sort
+        );
+    }
+}
