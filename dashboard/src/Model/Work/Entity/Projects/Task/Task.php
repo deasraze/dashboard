0fe3f10b4ca144672145a5cba28de265e4c08b00
deasraze <condecrom@gmail.com>
2021-08-24 10:46:11 +0300

    Added executors for tasks

--- dashboard/src/Model/Work/Entity/Projects/Task/Task.php
+++ dashboard/src/Model/Work/Entity/Projects/Task/Task.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace App\Model\Work\Entity\Projects\Task;
 
+use App\Model\Work\Entity\Members\Member\Id as MemberId;
 use App\Model\Work\Entity\Members\Member\Member;
 use App\Model\Work\Entity\Projects\Project\Project;
+use Doctrine\Common\Collections\ArrayCollection;
 use Webmozart\Assert\Assert;
 
 class Task
@@ -22,6 +24,10 @@ class Task
     private int $priority;
     private ?Task $parent = null;
     private Status $status;
+    /**
+     * @var ArrayCollection|Member[]
+     */
+    private $executors;
 
     public function __construct(
         Id $id,
@@ -43,6 +49,7 @@ class Task
         $this->progress = 0;
         $this->priority = $priority;
         $this->status = Status::new();
+        $this->executors = new ArrayCollection();
     }
 
     public function edit(string $name, ?string $content): void
@@ -120,6 +127,39 @@ class Task
         $this->status = $status;
     }
 
+    public function assignExecutor(Member $executor): void
+    {
+        if ($this->executors->contains($executor)) {
+            throw new \DomainException('Executor is already assigned.');
+        }
+
+        $this->executors->add($executor);
+    }
+
+    public function revokeExecutor(MemberId $id): void
+    {
+        foreach ($this->executors as $executor) {
+            if ($executor->getId()->isEqual($id)) {
+                $this->executors->removeElement($executor);
+
+                return;
+            }
+        }
+
+        throw new \DomainException('Executor is not assigned.');
+    }
+
+    public function hasExecutor(MemberId $id): bool
+    {
+        foreach ($this->executors as $executor) {
+            if ($executor->getId()->isEqual($id)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
     public function isNew(): bool
     {
         return $this->status->isNew();
@@ -183,5 +223,13 @@ class Task
     public function getStatus(): Status
     {
         return $this->status;
+    }
+
+    /**
+     * @return Member[]
+     */
+    public function getExecutors(): array
+    {
+        return $this->executors->toArray();
     }
 }
---
+++ dashboard/tests/Unit/Model/Work/Entity/Projects/Task/Executor/RevokeTest.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Unit\Model\Work\Entity\Projects\Task\Executor;
+
+use App\Tests\Builder\Work\Members\GroupBuilder;
+use App\Tests\Builder\Work\Members\MemberBuilder;
+use App\Tests\Builder\Work\Projects\ProjectBuilder;
+use App\Tests\Builder\Work\Projects\TaskBuilder;
+use PHPUnit\Framework\TestCase;
+
+class RevokeTest extends TestCase
+{
+    public function testSuccess(): void
+    {
+        $memberBuilder = new MemberBuilder();
+
+        $group = (new GroupBuilder())->build();
+        $member = $memberBuilder->build($group);
+        $project = (new ProjectBuilder())->build();
+        $task = (new TaskBuilder())->build($project, $member);
+
+        $executor = $memberBuilder->build($group);
+
+        $task->assignExecutor($executor);
+
+        self::assertTrue($task->hasExecutor($executor->getId()));
+
+        $task->revokeExecutor($executor->getId());
+
+        self::assertEquals([], $task->getExecutors());
+        self::assertFalse($task->hasExecutor($executor->getId()));
+    }
+
+    public function testNotFound(): void
+    {
+        $memberBuilder = new MemberBuilder();
+
+        $group = (new GroupBuilder())->build();
+        $member = $memberBuilder->build($group);
+        $project = (new ProjectBuilder())->build();
+        $task = (new TaskBuilder())->build($project, $member);
+
+        $executor = $memberBuilder->build($group);
+
+        $this->expectExceptionMessage('Executor is not assigned.');
+        $task->revokeExecutor($executor->getId());
+    }
+}
