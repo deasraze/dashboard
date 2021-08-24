eea95268533b25f8e19aba8e183974dc9cbd0181
deasraze <condecrom@gmail.com>
2021-08-24 20:41:15 +0300

    Added projects tasks id sequence

--- dashboard/src/Model/Work/Entity/Projects/Task/Task.php
+++ dashboard/src/Model/Work/Entity/Projects/Task/Task.php
@@ -22,6 +22,8 @@ class Task
     /**
      * @ORM\Id()
      * @ORM\Column(type="work_projects_task_id")
+     * @ORM\GeneratedValue(strategy="SEQUENCE")
+     * @ORM\SequenceGenerator(sequenceName="work_projects_tasks_seq", initialValue=1)
      */
     private Id $id;
     /**
