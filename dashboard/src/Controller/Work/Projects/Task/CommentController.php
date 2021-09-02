d5f87e62f11dda300aa0d4fb5d79d8b21eb6e46a
deasraze <condecrom@gmail.com>
2021-09-02 11:55:09 +0300

    Added comments

---
+++ dashboard/src/Controller/Work/Projects/Task/CommentController.php
@@ -0,0 +1,105 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Work\Projects\Task;
+
+use App\Annotation\Guid;
+use App\Controller\ErrorHandler;
+use App\Model\Comment\Entity\Comment\Comment;
+use App\Model\Comment\UseCase\Comment\Edit;
+use App\Model\Comment\UseCase\Comment\Remove;
+use App\Model\Work\Entity\Projects\Task\Task;
+use App\Security\Voter\Comment\CommentAccess;
+use App\Security\Voter\Work\Projects\TaskAccess;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/work/projects/tasks/{task_id}/comments", name="work.projects.tasks.comments", requirements={
+ *     "task_id"="\d+",
+ *     "id"=Guid::PATTERN
+ * })
+ * @ParamConverter("task", options={"id" = "task_id"})
+ */
+class CommentController extends AbstractController
+{
+    private ErrorHandler $errors;
+
+    public function __construct(ErrorHandler $errors)
+    {
+        $this->errors = $errors;
+    }
+
+    /**
+     * @Route("/{id}/edit", name=".edit")
+     */
+    public function edit(Task $task, Comment $comment, Request $request, Edit\Handler $handler): Response
+    {
+        $this->checkCommentIsForTask($task, $comment);
+        $this->denyAccessUnlessGranted(TaskAccess::VIEW, $task);
+        $this->denyAccessUnlessGranted(CommentAccess::MANAGE, $comment);
+
+        $command = Edit\Command::fromComment($comment);
+
+        $form = $this->createForm(Edit\Form::class, $command);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            try {
+                $handler->handle($command);
+
+                return $this->redirectToRoute('work.projects.tasks.show', ['id' => $task->getId()]);
+            } catch (\DomainException $e) {
+                $this->errors->handle($e);
+                $this->addFlash('error', $e->getMessage());
+            }
+        }
+
+        return $this->render('app/work/projects/tasks/comment/edit.html.twig', [
+            'project' => $task->getProject(),
+            'task' => $task,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/delete", name=".delete", methods={"POST"})
+     */
+    public function delete(Task $task, Comment $comment, Request $request, Remove\Handler $handler): Response
+    {
+        if (!$this->isCsrfTokenValid('delete-comment', $request->request->get('token'))) {
+            return $this->redirectToRoute('work.projects.tasks.show', ['id' => $task->getId()]);
+        }
+
+        $this->checkCommentIsForTask($task, $comment);
+        $this->denyAccessUnlessGranted(TaskAccess::VIEW, $task);
+        $this->denyAccessUnlessGranted(CommentAccess::MANAGE, $comment);
+
+        $command = new Remove\Command($comment->getId()->getValue());
+
+        try {
+            $handler->handle($command);
+
+            return $this->redirectToRoute('work.projects.tasks.show', ['id' => $task->getId()]);
+        } catch (\DomainException $e) {
+            $this->errors->handle($e);
+            $this->addFlash('error', $e->getMessage());
+        }
+
+        return $this->redirectToRoute('work.projects.tasks.show', ['id' => $task->getId()]);
+    }
+
+    private function checkCommentIsForTask(Task $task, Comment $comment): void
+    {
+        if (!(
+            Task::class === $comment->getEntity()->getType() &&
+            (int) $comment->getEntity()->getId() === $task->getId()->getValue()
+        )) {
+            throw $this->createNotFoundException();
+        }
+    }
+}
--- dashboard/src/Controller/Work/Projects/TasksController.php
+++ dashboard/src/Controller/Work/Projects/TasksController.php
@@ -6,6 +6,7 @@ namespace App\Controller\Work\Projects;
 
 use App\Annotation\Guid;
 use App\Controller\ErrorHandler;
+use App\Model\Comment\UseCase\Comment;
 use App\Model\Work\Entity\Members\Member\Member;
 use App\Model\Work\Entity\Projects\Task\Task;
 use App\Model\Work\UseCase\Projects\Task\ChildOf;
@@ -23,6 +24,7 @@ use App\Model\Work\UseCase\Projects\Task\Take;
 use App\Model\Work\UseCase\Projects\Task\TakeAndStart;
 use App\Model\Work\UseCase\Projects\Task\Type;
 use App\ReadModel\Work\Members\Member\MemberFetcher;
+use App\ReadModel\Work\Projects\Task\CommentFetcher;
 use App\ReadModel\Work\Projects\Task\Filter;
 use App\ReadModel\Work\Projects\Task\TaskFetcher;
 use App\Security\Voter\Work\Projects\TaskAccess;
@@ -496,10 +498,12 @@ class TasksController extends AbstractController
         Request $request,
         MemberFetcher $members,
         TaskFetcher $tasks,
+        CommentFetcher $comments,
         Status\Handler $statusHandler,
         Progress\Handler $progressHandler,
         Type\Handler $typeHandler,
-        Priority\Handler $priorityHandler
+        Priority\Handler $priorityHandler,
+        Comment\Create\Handler $commentHandler
     ): Response {
         $this->denyAccessUnlessGranted(TaskAccess::VIEW, $task);
 
@@ -567,15 +571,36 @@ class TasksController extends AbstractController
             }
         }
 
+        $commentCommand = new Comment\Create\Command(
+            $this->getUser()->getId(),
+            Task::class,
+            (string) $task->getId()->getValue()
+        );
+        $commentForm = $this->createForm(Comment\Create\Form::class, $commentCommand);
+        $commentForm->handleRequest($request);
+
+        if ($commentForm->isSubmitted() && $commentForm->isValid()) {
+            try {
+                $commentHandler->handle($commentCommand);
+
+                return $this->redirectToRoute('work.projects.tasks.show', ['id' => $task->getId()]);
+            } catch (\DomainException $e) {
+                $this->errors->handle($e);
+                $this->addFlash('error', $e->getMessage());
+            }
+        }
+
         return $this->render('app/work/projects/tasks/show.html.twig', [
             'project' => $task->getProject(),
             'task' => $task,
             'member' => $member,
             'children' => $tasks->childrenOf($task->getId()->getValue()),
+            'comments' => $comments->allForTask($task->getId()->getValue()),
             'statusForm' => $statusForm->createView(),
             'progressForm' => $progressForm->createView(),
             'typeForm' => $typeForm->createView(),
             'priorityForm' => $priorityForm->createView(),
+            'commentForm' => $commentForm->createView(),
         ]);
     }
 }
