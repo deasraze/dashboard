1da2652f0a7e002cf3d82a64db43df44b79313d5
deasraze <condecrom@gmail.com>
2021-09-05 11:08:42 +0300

    Added centrifuge js library

--- dashboard/assets/js/app.js
+++ dashboard/assets/js/app.js
@@ -10,3 +10,15 @@ import '../styles/app.scss';
 
 import 'bootstrap';
 import '@coreui/coreui';
+
+const Centrifuge = require('centrifuge');
+
+document.addEventListener('DOMContentLoader', function () {
+    const centrifuge = new Centrifuge('ws://localhost:8083/connection/websocket');
+
+    centrifuge.subscribe('alerts', function (message) {
+        console.log(message.data.message);
+    });
+
+    centrifuge.connect();
+});
