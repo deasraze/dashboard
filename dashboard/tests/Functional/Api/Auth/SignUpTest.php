5c1a78a85b206e0d07b0aad660a7fd7453ff9e98
deasraze <condecrom@gmail.com>
2021-09-06 11:50:50 +0300

    Added api sign up test

---
+++ dashboard/tests/Functional/Api/Auth/SignUpTest.php
@@ -0,0 +1,88 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Functional\Api\Auth;
+
+use App\Tests\Functional\DbWebTestCase;
+
+class SignUpTest extends DbWebTestCase
+{
+    private const URI = '/api/auth/signup';
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->client->setServerParameter('CONTENT_TYPE', 'application/json');
+    }
+
+    public function testGet(): void
+    {
+        $this->client->request('GET', self::URI);
+
+        $this->assertResponseStatusCodeSame(405);
+    }
+
+    public function testSuccess(): void
+    {
+        $this->client->request('POST', self::URI, [], [], [], \json_encode([
+            'first_name' => 'John',
+            'last_name' => 'Doe',
+            'email' => 'test-john@app.test',
+            'password' => 'password',
+        ]));
+
+        $this->assertResponseStatusCodeSame(201);
+        self::assertJson($content = $this->client->getResponse()->getContent());
+
+        $data = \json_decode($content, true);
+
+        self::assertEquals([], $data);
+    }
+
+    public function testNotValid(): void
+    {
+        $this->client->request('POST', self::URI, [], [], [], \json_encode([
+            'first_name' => '',
+            'last_name' => '',
+            'email' => 'invalid-email',
+            'password' => 'short',
+        ]));
+
+        $this->assertResponseStatusCodeSame(400);
+        self::assertJson($content = $this->client->getResponse()->getContent());
+
+        $data = \json_decode($content, true);
+
+        self::assertContains([
+            'violations' => [
+                ['propertyPath' => 'first_name', 'title' => 'This value should not be blank.'],
+                ['propertyPath' => 'last_name', 'title' => 'This value should not be blank.'],
+                ['propertyPath' => 'email', 'title' => 'This value is not a valid email address.'],
+                ['propertyPath' => 'password', 'title' => 'This value is too short. It should have 6 characters or more.'],
+            ],
+        ], $data);
+    }
+
+    public function testExist(): void
+    {
+        $this->client->request('POST', self::URI, [], [], [], \json_encode([
+            'first_name' => 'Tom',
+            'last_name' => 'Aiz',
+            'email' => 'existing-user@app.test',
+            'password' => 'password',
+        ]));
+
+        $this->assertResponseStatusCodeSame(400);
+        self::assertJson($content = $this->client->getResponse()->getContent());
+
+        $data = \json_decode($content, true);
+
+        self::assertContains([
+            'error' => [
+                'message' => 'User is already exists.',
+            ],
+        ], $data);
+    }
+}
