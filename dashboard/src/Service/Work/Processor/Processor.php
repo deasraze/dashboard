bf858b2d4b85ab52c8f85cb9d55286f01836573d
deasraze <condecrom@gmail.com>
2021-09-08 10:02:32 +0300

    Extracted work processor

--- dashboard/src/Service/Work/Processor/Processor.php
+++ dashboard/src/Service/Work/Processor/Processor.php
@@ -2,13 +2,12 @@
 
 declare(strict_types=1);
 
-namespace App\Twig\Extension\Work\Processor;
+namespace App\Service\Work\Processor;
 
-use App\Twig\Extension\Work\Processor\Driver\Driver;
+use App\Service\Work\Processor\Driver\Driver;
 use Twig\Extension\AbstractExtension;
-use Twig\TwigFilter;
 
-class ProcessorExtension extends AbstractExtension
+class Processor extends AbstractExtension
 {
     /**
      * @var Driver[]
@@ -18,13 +17,6 @@ class ProcessorExtension extends AbstractExtension
     public function __construct(iterable $drivers)
     {
         $this->drivers = $drivers;
-    }
-
-    public function getFilters(): array
-    {
-        return [
-            new TwigFilter('work_processor', [$this, 'process'], ['is_safe' => ['html']]),
-        ];
     }
 
     public function process(string $text): string
