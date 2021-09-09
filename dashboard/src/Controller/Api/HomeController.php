6c29bd29b19087594a705273d48210475e9e5f87
deasraze <condecrom@gmail.com>
2021-09-09 17:06:23 +0300

    Extracted error response annotations

--- dashboard/src/Controller/Api/HomeController.php
+++ dashboard/src/Controller/Api/HomeController.php
@@ -33,6 +33,19 @@ use Symfony\Component\Routing\Annotation\Route;
  *     type="http",
  *     securityScheme="basicAuth",
  *     scheme="basic"
+ * ),
+ * @OA\Schema(
+ *     schema="ErrorModel",
+ *     type="object",
+ *     @OA\Property(property="error", type="object", nullable=true,
+ *         @OA\Property(property="code", type="integer"),
+ *         @OA\Property(property="message", type="string"),
+ *     ),
+ *     @OA\Property(property="violations", type="array", nullable=true, @OA\Items(
+ *         type="object",
+ *         @OA\Property(property="propertyPath", type="string"),
+ *         @OA\Property(property="title", type="string"),
+ *     ))
  * )
  */
 class HomeController extends AbstractController
